6acb32adcfe2535214d5b832734bd6923275ef9a
Syed Faseeh <carxluvrx@gmail.com>
2024-06-16 19:25:08 +0500

    Update Main.js

--- Frontend/src/logged_out/components/Main.js
+++ Frontend/src/logged_out/components/Main.js
@@ -1,7 +1,7 @@
 import React, { memo, useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
-
+// this is testing
 import AOS from "aos/dist/aos";
 import withStyles from "@mui/styles/withStyles";
 import NavBar from "./navigation/NavBar";
